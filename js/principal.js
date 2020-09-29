fd2c82eaa665bc4f5ba42d65c8d58eb929b884ef
Raphael <raphaelscandura@hotmail.com>
2020-09-29 17:35:50 -0300

    Segundo capítulo completo

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,32 @@
 console.log("Arquivo externo de JS carregado com sucesso");
 
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiroPaciente");
+
+var pesoPaciente = paciente.querySelector(".info-peso");
+var peso = pesoPaciente.textContent;
+
+var alturaPaciente = paciente.querySelector(".info-altura");
+var altura = alturaPaciente.textContent;
+
+var imcPaciente = paciente.querySelector(".info-imc");
+
+var alturaValida = true;
+var pesoValido = true;
+
+if(peso <= 0 || peso >= 1000){
+    pesoValido = false;
+}
+
+if(altura <= 0 || altura >= 5){
+    alturaValida = false;
+}
+
+if(alturaValida && pesoValido){
+    var imc = peso / (altura * altura);
+    imcPaciente.textContent = imc;
+} else{
+    imcPaciente.textContent = "Valor de Peso ou Altura inválido!"
+}
