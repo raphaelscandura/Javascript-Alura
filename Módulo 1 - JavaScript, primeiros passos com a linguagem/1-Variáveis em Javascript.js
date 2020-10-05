a3924d34154c658778f626301edb42458fab2f9e
Raphael <raphaelscandura@hotmail.com>
2020-10-05 17:04:17 -0300

    Primeiro capítulo completo

---
+++ Módulo 1 - JavaScript, primeiros passos com a linguagem/1-Variáveis em Javascript.js
@@ -0,0 +1,15 @@
+console.log("Primeiro programa com JS usando o terminal PowerShell e Node");
+
+const idade = 23;
+
+console.log(idade);
+
+const idadeFutura = idade + 5;
+const idadeFutura1 = idade - 5;
+const idadeFutura2= idade * 5;
+const idadeFutura3 = idade / 5;
+
+console.log("Sua idade mais 5 é igual a ", idadeFutura);
+console.log("Sua idade menos 5 é igual a ", idadeFutura1);
+console.log("Sua idade vezes 5 é igual a ", idadeFutura2);
+console.log("Sua idade dividida por 5 é igual a ", idadeFutura3);
