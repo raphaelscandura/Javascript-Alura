eeef21fb19e5588635c0b8cdd2e290b6954c3f84
Raphael <raphaelscandura@hotmail.com>
2020-10-21 17:47:00 -0300

    Módulo cinco completo

--- Módulo 1 - JavaScript, primeiros passos com a linguagem/5-Atribuição de Variáveis.js
+++ Módulo 1 - JavaScript, primeiros passos com a linguagem/5-Atribuição de Variáveis.js
@@ -1,7 +1,7 @@
 console.log("Trabalhando com atribuição de variáveis");
 const idade = 29;
-const primeiroNome = "Ricardo";
-const sobrenome = "Bugan";
+const primeiroNome = "Raphael";
+const sobrenome = "Scandura";
 
 //console.log(nome + " " + sobrenome)
 console.log(primeiroNome, sobrenome)
@@ -11,4 +11,3 @@ let contador = 0
 contador = contador +1
 const nomeCompleto = primeiroNome + sobrenome;
 console.log(nomeCompleto);
-nomeCompleto =2;
